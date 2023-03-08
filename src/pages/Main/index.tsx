c3a2e4f89e18b1b4e4696e2f994a6aa623ad1744
Natalia Oliveira <nob.ufba@gmail.com>
2023-03-08 17:55:33 -0300

    first commit

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ola Mundo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
