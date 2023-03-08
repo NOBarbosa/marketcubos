28574e9fd127874e75593b7be969cecf80f5528e
Natalia Oliveira <nob.ufba@gmail.com>
2023-03-08 18:50:51 -0300

    routes management

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,10 @@
-import './styles.css';
+import s from './main.module.scss';
+
 
 function Main() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={s.container}>
         <h1>Ola Mundo</h1>
-      </header>
     </div>
   );
 }
